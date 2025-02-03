35c3a2d32809c92771ace6924e1d485a352fe4d4
BliriBerisha <bliri.berisha@student.uni-pr.edu>
2025-02-03 03:16:21 +0100

    added payment + fixed payment bugs

--- shop/shop.cpp
+++ shop/shop.cpp
@@ -31,21 +31,30 @@ struct Game {
     GameEdition edition;
     string description;
     double price;
+    status purchaseStatus;
 };
 
+
+
+enum status {
+    NOT_PURCHASED,
+    PURCHASED
+};
 
 Game games[] = {
-    {"The Elder Scrolls V: Skyrim", SPECIAL, "Special Edition (with all DLCs)", 49.99},
-    {"Cyberpunk 2077", COLLECTORS, "Collector's Edition", 59.99},
-    {"Red Dead Redemption 2", ULTIMATE, "Ultimate Edition", 59.99},
-    {"Destiny 2", COMPLETE, "Forsaken (with all expansions)", 39.99},
-    {"World of Warcraft", COMPLETE, "Shadowlands (with a year's subscription)", 49.99},
-    {"Call of Duty: Modern Warfare", SPECIAL, "Battle Pass & Special Editions", 49.99},
-    {"Final Fantasy XIV", COMPLETE, "Complete Collector’s Edition", 39.99},
-    {"The Witcher 3: Wild Hunt", GOTY, "Game of the Year Edition (with expansions)", 39.99},
-    {"Star Citizen", ALPHA, "Various Pack Upgrades", 39.99},
-    {"Halo: The Master Chief Collection", SPECIAL, "Special Editions", 39.99}
+    {"The Elder Scrolls V: Skyrim", SPECIAL, "Special Edition (with all DLCs)", 49.99, NOT_PURCHASED},
+    {"Cyberpunk 2077", COLLECTORS, "Collector's Edition", 59.99, NOT_PURCHASED},
+    {"Red Dead Redemption 2", ULTIMATE, "Ultimate Edition", 59.99, NOT_PURCHASED},
+    {"Destiny 2", COMPLETE, "Forsaken (with all expansions)", 39.99, NOT_PURCHASED},
+    {"World of Warcraft", COMPLETE, "Shadowlands (with a year's subscription)", 49.99, NOT_PURCHASED},
+    {"Call of Duty: Modern Warfare", SPECIAL, "Battle Pass & Special Editions", 49.99, NOT_PURCHASED},
+    {"Final Fantasy XIV", COMPLETE, "Complete Collector’s Edition", 39.99, NOT_PURCHASED},
+    {"The Witcher 3: Wild Hunt", GOTY, "Game of the Year Edition (with expansions)", 39.99, NOT_PURCHASED},
+    {"Star Citizen", ALPHA, "Various Pack Upgrades", 39.99, NOT_PURCHASED},
+    {"Halo: The Master Chief Collection", SPECIAL, "Special Editions", 39.99, NOT_PURCHASED}
 };
+
+
 
 
  
@@ -124,6 +133,7 @@ void showShop() {
 
 void showGames() {
     int gameChoice;
+    int countTrue = 0;
     char paymentChoice;
     gameMenu:
     cout << "Games available in the shop:" << endl;
@@ -136,7 +146,7 @@ void showGames() {
     cout << "Enter the number of the game you want to buy: ";
     cout << endl;
     cin >> gameChoice;
-
+   
     if (gameChoice < 1 || gameChoice > sizeof(games)/sizeof(games[0])) {
         cout << "Invalid choice!" << endl;
         cout << "Returning to back..." << endl;
@@ -144,6 +154,9 @@ void showGames() {
 
         goto gameMenu;
     }
+
+    
+
 
     cout << "--------------------------------" << endl;
     cout << "You have selected " << games[gameChoice-1].title << " - " << games[gameChoice-1].description << " - " << games[gameChoice-1].price << endl;
@@ -156,9 +169,34 @@ void showGames() {
     switch (paymentChoice) {
         case 'Y':
         case 'y':
+            games[gameChoice-1].purchaseStatus = PURCHASED;
             cout << "You have selected Yes!" << endl;
             cout << "Redirecting to payment..." << endl;
             this_thread::sleep_for(chrono::milliseconds(1000));
+
+            if (games[gameChoice-1].price > bank) {
+                cout << "You don't have enough money to buy this game!" << endl;
+                cout << "Returning to back..." << endl;
+                this_thread::sleep_for(chrono::milliseconds(1000));
+                goto gameMenu;
+            }
+            //buying game
+             else {
+                if (games[gameChoice-1].purchaseStatus == PURCHASED) {
+                    cout << "You have already bought this game!" << endl;
+                    cout << "Returning to back..." << endl;
+                    this_thread::sleep_for(chrono::milliseconds(1000));
+                    goto gameMenu;
+                }
+                bank -= games[gameChoice-1].price;
+                cout << "You have successfully bought " << games[gameChoice-1].title << "!" << endl;
+                cout << "Your remaining balance is: " << bank << endl;
+                cout << "Returning to back..." << endl;
+                
+                this_thread::sleep_for(chrono::milliseconds(1000));
+                goto gameMenu;
+            }
+            
             break;
         case 'N':
         case 'n':
